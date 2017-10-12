eb877bcbf2fc6c9dcb423eddeeec6e56ba6afea2
Cian Byrne <wallarug@bitbucket.org>
2017-10-12 22:31:17 +1100

    renamed a whole heap of files in the "testing" folder so that they make a bit more sense without requiring documentation

--- src/testing/Joystick Tester.py
+++
@@ -1,8 +0,0 @@
-import pygame
-pygame.init()
-j = pygame.joystick.Joystick(0)
-j.init()
-joycount = pygame.joystick.get_count()
-joy = j.get_name()
-print joycount
-print joy
--- src/testing/Servo_maths_formula.py
+++
@@ -1,8 +0,0 @@
-
-print "Welcome to Servo testing Script!"
-
-while True:
-    x = input("Input 'x' value between 0 - 1: ")
-    servo_turn = 90 + (45*x)
-    servo_turn = round(servo_turn, 0)
-    print servo_turn
