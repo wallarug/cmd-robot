69fb06c9379241f916bf5e6effb4209b167cce6a
Cian Byrne <wallarug@bitbucket.org>
2017-10-12 22:17:47 +1100

    added adafruit charlcd code

---
+++ src/lcd/Adafruit_CharLCD_IPclock_example.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from Adafruit_CharLCD import Adafruit_CharLCD
+from subprocess import * 
+from time import sleep, strftime
+from datetime import datetime
+import RPi.GPIO as g
+
+
+lcd = Adafruit_CharLCD()
+
+cmd = "ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+
+lcd.begin(16,1)
+
+def run_cmd(cmd):
+        p = Popen(cmd, shell=True, stdout=PIPE)
+        output = p.communicate()[0]
+        return output
+
+try:
+	while 1:
+		lcd.clear()
+		ipaddr = run_cmd(cmd)
+		lcd.message(datetime.now().strftime('%b %d  %H:%M:%S\n'))
+		lcd.message('IP %s' % ( ipaddr ) )
+		sleep(1)
+except KeyboardInterrupt:
+	g.cleanup()
+	lcd.clear()
